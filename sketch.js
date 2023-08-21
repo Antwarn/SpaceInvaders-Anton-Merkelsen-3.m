e19abfa6140827d5e8f8433a3a6616541cc4cd7d
Anton Merkelsen <anton.v.merkelsen@gmail.com>
2023-08-21 15:19:38 +0200

    Aliel class lavet

--- sketch.js
+++ sketch.js
@@ -1,7 +1,37 @@
+let alien0;
+let alien1;
+function preload() {
+  alien0 = loadImage('alien0.png');
+  alien1 = loadImage('alien1.png')
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(0);
+  alien = new alien(200,300);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  alien.draw();
+  Invader.display();
 }
+
+class alien{
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw() {
+    image(alien0, this.x, this.y, 40, 40)
+  }
+}
+
+class ship{
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
