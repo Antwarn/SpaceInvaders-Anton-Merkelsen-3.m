4a816b7db4336a79a8b22650f2935e071ca61758
Anton Merkelsen <55212514+Antwarn@users.noreply.github.com>
2023-09-16 13:39:26 +0200

    Bullets virker
    Man kan nu skyde med playeren, ved at trykke på arrow_up

--- sketch.js
+++ sketch.js
@@ -1,19 +1,30 @@
 let player;
+let bullets = [];
 
 function setup() {
   createCanvas(400, 400);
-  player = new Player(); // 
+  player = new Player();
 }
 
-
-function draw() { // Kalder de metoder der skal bruges for at vise og bevæge mit rumskib
+function draw() {
   background(0);
   player.show();
   player.move();
+  player.fire();
+
+  for (let i = bullets.length - 1; i >= 0; i--) {
+    bullets[i].draw();
+    bullets[i].update();
+
+    if (bullets[i].y < 0) {
+      bullets.splice(i, 1);
+    }
+  }
 }
 
 class Player {
-  constructor() { // I Constructoren har jeg tilføjet alle de variabler playern skal bruge
+  constructor() {
+    this.width = 40;
     this.height = 20;
     this.x = width / 2 - this.width;
     this.y = height - 50;
@@ -21,19 +32,41 @@ class Player {
   }
 
   show() {
-    fill(220)
-    rect(this.x, this.y, this.width, this.height)
+    fill(220);
+    rect(this.x, this.y, this.width, this.height);
   }
 
   move() {
-    if (keyIsDown(LEFT_ARROW) && this.x >= 0 ) {
-      // Tjekker om spillern bevæger sig mod venstre og ikke ud af skærmen
+    if (keyIsDown(LEFT_ARROW) && this.x >= 0) {
       this.x -= this.speed;
     }
     if (keyIsDown(RIGHT_ARROW) && this.x <= width - this.width) {
       this.x += this.speed;
     }
   }
+
+  fire() {
+    if (keyIsDown(38)) { // Check if the spacebar is pressed
+      bullets.push(new Bullet(this.x + this.width / 2, this.y));
+    }
+  }
 }
 
+class Bullet {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.HasNotHit = true;
+  }
 
+  draw() {
+    if (this.HasNotHit) {
+      fill(255, 0, 0);
+      circle(this.x, this.y, 7);
+    }
+  }
+
+  update() {
+    this.y -= 2;
+  }
+}
